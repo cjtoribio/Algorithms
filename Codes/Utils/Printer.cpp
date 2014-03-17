5be6cf2dbe4fd02f38a924f813061e67a2ca288e
cjtoribio <cjtoribio@gmail.com>
2014-03-17 02:07:55 +0000

    Printer.cpp created online with Bitbucket

---
+++ Codes/Utils/Printer.cpp
@@ -0,0 +1,33 @@
+struct Printer
+{
+	template<class T>
+	static void print(pair<T,T> P)
+	{
+		printf("(%d,%d)",P.first,P.second);
+	}
+	static void print(int n)
+	{
+		printf("%d",n);
+	}
+	template<class T>
+	static void print(vector<T> V)
+	{
+		printf("{");
+		for(int i = 0; i < V.size(); ++i){
+			if(i > 0)printf(", ");
+			print(V[i]);
+		}
+		printf("}\n");
+
+	}
+	template<class T>
+	static void print(vector<vector<T> > V)
+	{
+		printf("{\n");
+		for(int i = 0; i < V.size(); ++i){
+			printf("  [%d]\t",i);
+			print(V[i]);
+		}
+		printf("}\n");
+	}
+};
