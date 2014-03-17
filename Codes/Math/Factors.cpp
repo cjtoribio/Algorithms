1b7c36a51fcb0016c60b4b13af048ba80e7e8c4f
cjtoribio <cjtoribio@gmail.com>
2014-03-17 02:45:54 +0000

    Factors.cpp created online with Bitbucket

---
+++ Codes/Math/Factors.cpp
@@ -0,0 +1,18 @@
+struct Factors
+{
+	static vector<int> factorize(int N)
+	{
+		vector<int> ANS; ANS.reserve(200);
+		while(N % 2 == 0) ANS.push_back(2), N /= 2;
+		for(int i = 3; i*i <= N && N > 1; i += 2)
+		{
+			while(N % i == 0)
+			{
+				ANS.push_back(i);
+				N /= i;
+			}
+		}
+		if(N > 1)ANS.push_back(N);
+		return ANS;
+	}
+};
