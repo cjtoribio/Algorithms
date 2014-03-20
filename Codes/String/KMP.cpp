0589dce45c65a08f21cd84daec70c886d655585b
cjtoribio <cjtoribio@gmail.com>
2014-03-20 23:43:31 +0000

    KMP.cpp created online with Bitbucket

---
+++ Codes/String/KMP.cpp
@@ -0,0 +1,33 @@
+struct KMP
+{
+	string needle;
+	vector<int> T;
+	KMP(const string needle)
+	{
+		this->needle = needle;
+		T = vector<int>(needle.size() + 1);
+		int i = 0 , j = -1;
+		T[0] = -1;
+		while(i < needle.size())
+		{
+			while(j >= 0 && needle[i] != needle[j])j = T[j];
+			T[++i] = ++j;
+		}
+	}
+	vector<int> match(const string hay)
+	{
+		vector<int> V;
+		int i = 0 , j = 0;
+		while(i < hay.size())
+		{
+			while(j >= 0 && hay[i] != needle[j])j = T[j];
+			++i;++j;
+			if(j == needle.size())
+			{
+				V.push_back(i - j);
+				j = T[j];
+			}
+		}
+		return V;
+	}
+};
