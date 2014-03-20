0f0745de24f753dce883e0d9895cbd902e6d4bd6
cjtoribio <cjtoribio@gmail.com>
2014-03-20 23:32:46 +0000

    StringUtils.cpp created online with Bitbucket

---
+++ Codes/String/StringUtils.cpp
@@ -0,0 +1,12 @@
+struct StringUtils
+{
+	static string getRandString(int maxSize = 0)
+	{
+		if(maxSize == 0)maxSize = 100;
+		string ret = "";
+		int sz = rand() % maxSize + 1;
+		for(int i = 0; i < sz; ++i)
+			ret += rand()%26 + 'a';
+		return ret;
+	}
+};
