106cd8501502cf3478750455a2a9dca554a04797
cjtoribio <cjtoribio@gmail.com>
2014-03-22 15:30:11 +0000

    DisjointSet.cpp created online with Bitbucket

---
+++ Codes/Trees/DisjointSet.cpp
@@ -0,0 +1,22 @@
+struct DisjointSet
+{
+	vector<int> R , P;
+	DisjointSet(int N)
+	{
+		R = vector<int>(N,0);
+		P = vector<int>(N,-1);
+	}
+	int find(int x)
+	{
+		return P[x] == -1 ? x : (P[x] = find(P[x]));
+	}
+	bool join(int x,int y)
+	{
+		x = find(x); y = find(y);
+		if(x == y)return false;
+		if(R[x] > R[y]) P[y] = x;
+		else P[x] = y;
+		R[y] += R[x] == R[y];
+		return true;
+	}
+};
