864f5201f82e84fe6ff31d20bd2a664334cd5046
cjtoribio <cjtoribio@gmail.com>
2014-05-05 14:59:43 +0000

    MincostMaxflow[AdjMatrix].cpp edited online with Bitbucket

--- Codes/Graph/MincostMaxflow[AdjMatrix].cpp
+++ Codes/Graph/MincostMaxflow[AdjMatrix].cpp
@@ -24,79 +24,65 @@
 
 using namespace std;
 
-typedef vector<int> VI;
-typedef vector<VI> VVI;
-typedef long long L;
-typedef vector<L> VL;
+typedef long long Long;
+typedef vector<Long> VL;
 typedef vector<VL> VVL;
-typedef pair<int, int> PII;
 typedef vector<PII> VPII;
-
-const L INF = numeric_limits<L>::max() / 4;
-
+const Long INF = numeric_limits<Long>::max() / 4;
 struct MinCostMaxFlow {
-  int N;
-  VVL cap, flow, cost;
-  VI found;
-  VL dist, pi, width;
-  VPII dad;
-
-  MinCostMaxFlow(int N) : 
-    N(N), cap(N, VL(N)), flow(N, VL(N)), cost(N, VL(N)), 
-    found(N), dist(N), pi(N), width(N), dad(N) {}
-  
-  void AddEdge(int from, int to, L cap, L cost) {
-    this->cap[from][to] = cap;
-    this->cost[from][to] = cost;
-  }
-  
-  void Relax(int s, int k, L cap, L cost, int dir) {
-    L val = dist[s] + pi[s] - pi[k] + cost;
-    if (cap && val < dist[k]) {
-      dist[k] = val;
-      dad[k] = make_pair(s, dir);
-      width[k] = min(cap, width[s]);
-    }
-  }
-
-  L Dijkstra(int s, int t) {
-    fill(found.begin(), found.end(), false);
-    fill(dist.begin(), dist.end(), INF);
-    fill(width.begin(), width.end(), 0);
-    dist[s] = 0;
-    width[s] = INF;
-    
-    while (s != -1) {
-      int best = -1;
-      found[s] = true;
-      for (int k = 0; k < N; k++) {
-        if (found[k]) continue;
-        Relax(s, k, cap[s][k] - flow[s][k], cost[s][k], 1);
-        Relax(s, k, flow[k][s], -cost[k][s], -1);
-        if (best == -1 || dist[k] < dist[best]) best = k;
-      }
-      s = best;
-    }
-
-    for (int k = 0; k < N; k++)
-      pi[k] = min(pi[k] + dist[k], INF);
-    return width[t];
-  }
-
-  pair<L, L> GetMaxFlow(int s, int t) {
-    L totflow = 0, totcost = 0;
-    while (L amt = Dijkstra(s, t)) {
-      totflow += amt;
-      for (int x = t; x != s; x = dad[x].first) {
-        if (dad[x].second == 1) {
-          flow[dad[x].first][x] += amt;
-          totcost += amt * cost[dad[x].first][x];
-        } else {
-          flow[x][dad[x].first] -= amt;
-          totcost -= amt * cost[x][dad[x].first];
-        }
-      }
-    }
-    return make_pair(totflow, totcost);
-  }
+	int N;
+	VVL cap, flow, cost;
+	VL dist, pi, width , found;
+	VPII P;
+	MinCostMaxFlow(int N) {
+		this->N = N;
+		cap = flow = cost= VVL(N,VL(N));
+		found = dist = pi = width = VL(N);
+		P = VPII(N);
+	}
+	void AddEdge(int from, int to, Long cap, Long cost) {
+		this->cap[from][to] = cap;
+		this->cost[from][to] = cost;
+	}
+	Long Dijkstra(int s, int t) {
+		fill(found.begin(), found.end(), false);
+		fill(dist.begin(), dist.end(), INF);
+		fill(width.begin(), width.end(), 0);
+		dist[s] = 0;
+		width[s] = INF;
+		priority_queue<PII> PQ;
+		PQ.push(PII(0, s));
+		while (PQ.size()) {
+			int u = PQ.top().second;
+			PQ.pop();
+			if (found[u]) continue;
+			found[u] = true;
+			for (int k = 0; k < N; k++) {
+				if (found[k]) continue;
+				int ca, nd;
+				ca = cap[u][k] - flow[u][k], nd = dist[u] + pi[u] - pi[k] + cost[u][k];
+				if (ca && nd < dist[k])
+					dist[k] = nd, width[k] = ca, PQ.push(PII(-nd, k)), P[k] = PII(u, 1);
+				ca = -flow[k][u], nd = dist[u] + pi[u] - pi[k] - cost[k][u];
+				if (ca && nd < dist[k])
+					dist[k] = nd, width[k] = ca, PQ.push(PII(-nd, k)), P[k] = PII(u, -1);
+			}
+		}
+		for (int k = 0; k < N; k++)
+			pi[k] = min(pi[k] + dist[k], INF);
+		return width[t];
+	}
+	pair<Long, Long> GetMaxFlow(int s, int t) {
+		Long totflow = 0, totcost = 0;
+		while (Long amt = Dijkstra(s, t)) {
+			totflow += amt;
+			for (int x = t; x != s; x = P[x].first) {
+				int d = P[x].second , u = P[x].first , v = x;
+				if(d==-1)swap(u,v);
+				flow[u][v] += amt * d;
+				totcost += amt * cost[u][v] * d;
+			}
+		}
+		return make_pair(totflow, totcost);
+	}
 };
