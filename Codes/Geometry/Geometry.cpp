c23302d8502a31aa4f3a257171bd3c578d2733f7
cjtoribio <cjtoribio@gmail.com>
2015-06-01 19:55:54 +0000

    Geometry.cpp edited online with Bitbucket

--- Codes/Geometry/Geometry.cpp
+++ Codes/Geometry/Geometry.cpp
@@ -48,6 +48,25 @@ template<class INT> struct Poin {
 };
 typedef Poin<double> Point;
 
+Point incenter(Point A,Point B,Point C){
+	double a = B.distTo(C), b = A.distTo(C), c = A.distTo(B);
+	return (A*a + B*b + C*c) * (1/(a+b+c));
+}
+
+// http://recursostic.educacion.es/descartes/web/materiales_didacticos/OrtoCircun/Ortocentro.htm
+Point orthocenter(Point A, Point B, Point C){
+	Point CA = A-C, AB = B-A, BC = C-B;
+	Point pBC(-BC.y, BC.x);
+	double t = -(CA*AB)/(pBC*AB);
+	return A + pBC * t;
+}
+
+//http://recursostic.educacion.es/descartes/web/materiales_didacticos/OrtoCircun/Circuncentro.htm
+Point circuncenter(Point A, Point B, Point C){
+	Point mAB = (B-A)*0.5 + A, mAC = (C-A)*0.5 + A, mBC = (C-B)*0.5 + B;
+	return orthocenter(mAB,mAC,mBC);
+}
+
 //
 Point Circle3Points(Point A, Point B, Point C) {
 	//Find the Center of a Circle that has points A,B,C 
