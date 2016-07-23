0766fe8f47006d1b1d1b3ae171c44a36bc7b73de
cjtoribio <cjtoribio@gmail.com>
2016-07-23 18:41:43 +0000

    ModInt.cpp edited online with Bitbucket

--- Codes/Math/ModInt.cpp
+++ Codes/Math/ModInt.cpp
@@ -1,4 +1,3 @@
-
 template<Long MOD> 
 struct ModInt {
 	Long n;
@@ -7,25 +6,49 @@ struct ModInt {
 		if(this->n < 0 || this->n >= MOD)this->n %= MOD;
 		if(this->n < 0)this->n = (this->n + MOD) % MOD;
 	}
-	ModInt operator+(const ModInt &M)const{
+	ModInt<MOD> operator+(const ModInt<MOD> &M)const{
 		ModInt r = (n + M.n);
 		if(r.n >= MOD)r.n -= MOD;
 		return r;
 	}
-	ModInt operator-(const ModInt &M)const{
-		ModInt r = (n - M.n) % MOD;
+	ModInt<MOD> operator-(const ModInt<MOD> &M)const{
+		ModInt<MOD> r = (n - M.n) % MOD;
 		if(r.n < 0)r.n += MOD;
 		return r;
 	}
-	ModInt operator*(const ModInt &M)const{
+	ModInt<MOD> operator*(const ModInt<MOD> &M)const{
 		return (1LL * n * M.n) % MOD;
 	}
-	ModInt operator+=(const ModInt &M){
+	ModInt<MOD> operator+=(const ModInt<MOD> &M){
 		return this->n = ((*this)+(M)).n;
 	}
-	ModInt operator-=(const ModInt &M){
+	ModInt<MOD> operator-=(const ModInt<MOD> &M){
 		return this->n = ((*this)-(M)).n;
+	}
+	ModInt<MOD> operator/(const ModInt<MOD> &B)const
+	{
+		Long a = B.n, b = MOD;
+		Long r = a , o_r = b;
+		Long s = 0 , o_s = 1;
+		Long t = 1 , o_t = 0;
+		while(r != 0)
+		{
+			Long q = o_r / r;
+			Long tem;
+			
+			tem = r;
+			r = o_r - r * q;
+			o_r = tem;
+			
+			tem = o_s;
+			o_s = o_s - s * q;
+			o_s = tem;
+			
+			tem = t;
+			t = o_t - t * q;
+			o_t = tem;
+		}
+		return (*this) * ModInt(o_t);
 	}
 };
 typedef ModInt<1000000007> mint;
- 
