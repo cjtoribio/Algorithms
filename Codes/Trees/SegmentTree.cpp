c51bb0890a7465c29b444901f2f140f1cb90786e
cjtoribio <cjtoribio@gmail.com>
2016-08-28 00:23:46 +0000

    SegmentTree.cpp edited online with Bitbucket

--- Codes/Trees/SegmentTree.cpp
+++ Codes/Trees/SegmentTree.cpp
@@ -10,36 +10,41 @@
 #include <vector>
 using namespace std;
 
-struct NodeInit {
-	char c;
-	NodeInit(char c=' '){
-		this->c = c;
-	}
-};
-struct Node
+
+struct MinVal
 {
-	int op, io, cl, ic, carry , sz;
+	struct Init {
+		int val, id;
+		Init(int val=0, int id=0){
+			this->val = val;
+			this->id  = id;
+		}
+	};
+	int mId, mV, carry , sz;
 	bool HasCarry;
-	Node(){
+	MinVal(){
+		this->mId = 0;
+		this->mV = 0;
 		this->carry = 0;
 		this->sz = 1;
 		HasCarry = 0;
 	}
-	Node(NodeInit n) {
-		this->op = n.c == '(';
-		this->cl = n.c == ')';
-		this->io = n.c == ')';
-		this->ic = n.c == '(';
+	MinVal(Init n) {
+		mId = n.id;
+		mV  = n.val;
 		this->carry = 0;
 		this->sz = 1;
 		HasCarry = 0;
 	}
-	Node operator+(const Node &N)const {
-		Node ret;
-		ret.op = N.op+max(0,op-N.cl); 
-		ret.cl = cl+max(0,N.cl-op);
-		ret.io = N.io+max(0,io-N.ic);
-		ret.ic = ic+max(0,N.ic-io);
+	MinVal operator+(const MinVal &N)const {
+		MinVal ret;
+		if(mV <= N.mV){
+			ret.mV = mV;
+			ret.mId = mId;
+		}else{
+			ret.mV = N.mV;
+			ret.mId = N.mId;
+		}
 		ret.carry = 0;
 		ret.HasCarry = 0;
 		ret.sz = this->sz + N.sz;
@@ -47,32 +52,31 @@ struct Node
 	}
 	void update(int val)
 	{
-		if(!val)return;
 		this->HasCarry = 1;
-		this->carry ^= 1;
-		swap(io,op);
-		swap(ic,cl);
+		this->carry += val;
+		this->mV += val;
 	}
 	void clear(){
 		this->HasCarry = 0;
 		this->carry = 0;
 	}
 };
+
+template<class T>
 struct SegmentTree
 {
-	Node *V;
+	vector<T> V;
 	int N;
 	SegmentTree(int N)
 	{
-		this->V = new Node[4 * N];
+		this->V = vector<T>(4*N);
 		this->N = N;
 	}
-	~SegmentTree(){ delete [] this->V; }
-	void create(vector<NodeInit> &VEC,int n = 1,int b = 0,int e = -1)
+	void create(vector<typename T::Init> &VEC,int n = 1,int b = 0,int e = -1)
 	{
 		if(e == -1)e = N - 1;
 		if(b == e){
-			V[n] = Node(VEC[b]);
+			V[n] = T(VEC[b]);
 		}
 		else
 		{
@@ -81,7 +85,7 @@ struct SegmentTree
 			V[n] = V[2*n] + V[2*n+1];
 		}
 	}
-	Node query(int i,int j, int n = 1,int b = 0,int e = -1)
+	T query(int i,int j, int n = 1,int b = 0,int e = -1)
 	{
 		if(e == -1)e = N - 1;
 		if(i <= b && e <= j)return V[n];
@@ -124,26 +128,3 @@ struct SegmentTree
 
 	}
 };
-
-//int main() {
-//	int SZ = 10;
-//	SegmentTree ST(SZ);
-//	for(int i = 0; i < 10; ++i)
-//	{
-//		ST.update(i,i,(92384*i)%10);
-//		cout << ST.query(5,8).val << endl;
-//		for(int j = 0; j < SZ; ++j)
-//			cout << ST.query(j,j).val << " ";
-//		cout << endl;
-//	}
-//	return 0;
-//}
-
-
-
-
-
-
-
-
-
