b337d26be2d5bb28d1f1599c954045b85d10be29
cjtoribio <cjtoribio@gmail.com>
2016-09-22 05:50:54 +0000

    ThirdPointLeft.cpp edited online with Bitbucket

--- Codes/Geometry/ThirdPointLeft.cpp
+++ Codes/Geometry/ThirdPointLeft.cpp
@@ -2,6 +2,7 @@ Point thirdPointLeft(Point A,Point B, Double ac, Double bc){
 	Point AB = B-A;
 	Double ab = AB.mag();
 	Double s = (ac+ab+bc)/2;
+	// assert(s > ac && s > ab && s > bc);
 	Double area = sqrt(s*(s-ab))*sqrt((s-bc)*(s-ac));
 	Double h = 2*area / ab;
 	Double ax = sqrt(ac*ac - h*h);
