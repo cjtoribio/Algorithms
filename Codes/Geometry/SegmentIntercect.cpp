25b52824147ce6beab4f357caa1980bc0feed9cf
cjtoribio <cjtoribio@gmail.com>
2016-09-22 04:13:21 +0000

    SegmentIntercect.cpp created online with Bitbucket

---
+++ Codes/Geometry/SegmentIntercect.cpp
@@ -0,0 +1,14 @@
+/*
+(A-C + (B-A)*t) ^ (D-C) = 0
+(B-A -> m : D-C -> n)
+(a.x + t*m.x)*n.y - (a.y + t*m.y)*n.x = 0
+a.x*n.y - a.y*n.x + t*(m^n) = 0
+t*(m^n) = n^a
+t = - ((a-c)^n) / (m^n)
+ */
+Point segIntercept(const Point A, const Point B, const Point C, const Point D){
+	Point m = (B-A);
+	Point n = (D-C);
+	double t = - ((A-C)^n) / (m^n);
+	return A + m*t;
+}
