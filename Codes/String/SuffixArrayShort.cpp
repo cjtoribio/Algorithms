511b4a1fc653a41f8a11a777e74004ad25bbcea4
cjtoribio NA <cjtoribio@gmail.com>
2017-09-26 17:39:57 +0000

    SuffixArrayShort.cpp edited online with Bitbucket

--- Codes/String/SuffixArrayShort.cpp
+++ Codes/String/SuffixArrayShort.cpp
@@ -14,10 +14,10 @@ struct SuffixArray {
 		VI freq(maxRank + 2), nSA(N);
 		for (int i = 0; i < N; ++i)
 			freq[vrank(i)]++;
-		for (int i = 0, sum = 0, t; i < freq.size(); ++i)
-			t = freq[i], freq[i] = sum, sum += t;
-		for (int i = 0, p, m; i < N; ++i)
-			nSA[freq[vrank(i)]++] = SA[i];
+		for (int i = 1; i < freq.size(); ++i)
+			freq[i] += freq[i-1];
+		for (int i = N-1, p, m; i >= 0; --i)
+			nSA[--freq[vrank(i)]] = SA[i];
 		SA = move(nSA);
 	}
 	void BuildSA() {
@@ -37,6 +37,103 @@ struct SuffixArray {
 			}
 			RA = move(nRA);
 		}
+	}
+	void BuildSA2(){
+		int *T = new int[N+3];
+		int *SA= new int[N+3];
+		for(int i = 0; i < A.size(); ++i)
+			T[i] = A[i];
+		T[N] = T[N+1] = T[N+2] = 0;
+		suffixArray(T, SA, N, 256);
+		for(int i = 0; i < N; ++i)
+			RA[ SA[i] ] = i;
+		for(int i = 0; i < N; ++i)
+			this->SA[i] = SA[i];
+	}
+	inline bool leq(int a1, int a2, int b1, int b2) {
+		return (a1 < b1 || (a1 == b1 && a2 <= b2));
+	}
+	inline bool leq(int a1, int a2, int a3, int b1, int b2, int b3) {
+		return (a1 < b1 || (a1 == b1 && leq(a2, a3, b2, b3)));
+	}
+	static void radixPass(int* a, int* b, int* r, int n, int K) {
+		int* c = new int[K + 1]; 
+		for (int i = 0; i <= K; i++)
+			c[i] = 0;  
+		for (int i = 0; i < n; i++){
+			c[r[a[i]]]++;  
+		}
+		for (int i = 1; i <= K; i++)
+			c[i] += c[i-1];
+		for (int i = n-1; i >= 0; --i)
+			b[--c[r[a[i]]]] = a[i];
+		delete[] c;
+	}
+	void suffixArray(int* T, int* SA, int n, int K) {
+		int n0 = (n + 2) / 3, n1 = (n + 1) / 3, n2 = n / 3, n02 = n0 + n2;
+		int* R = new int[n02 + 3];
+		R[n02] = R[n02 + 1] = R[n02 + 2] = 0;
+		int* SA12 = new int[n02 + 3];
+		SA12[n02] = SA12[n02 + 1] = SA12[n02 + 2] = 0;
+		int* R0 = new int[n0];
+		int* SA0 = new int[n0];
+		for (int i = 0, j = 0; i < n + (n0 - n1); i++)
+			if (i % 3 != 0)
+				R[j++] = i;
+		radixPass(R, SA12, T + 2, n02, K);
+		radixPass(SA12, R, T + 1, n02, K);
+		radixPass(R, SA12, T, n02, K);
+		int name = 0, c0 = -1, c1 = -1, c2 = -1;
+		for (int i = 0; i < n02; i++) {
+			if (T[SA12[i]] != c0 || T[SA12[i] + 1] != c1
+					|| T[SA12[i] + 2] != c2) {
+				name++;
+				c0 = T[SA12[i]];
+				c1 = T[SA12[i] + 1];
+				c2 = T[SA12[i] + 2];
+			}
+			if (SA12[i] % 3 == 1) {
+				R[SA12[i] / 3] = name;
+			}
+			else {
+				R[SA12[i] / 3 + n0] = name;
+			}
+		}
+		if (name < n02) {
+			suffixArray(R, SA12, n02, name);
+			for (int i = 0; i < n02; i++)
+				R[SA12[i]] = i + 1;
+		} else
+			for (int i = 0; i < n02; i++)
+				SA12[R[i] - 1] = i;
+		for (int i = 0, j = 0; i < n02; i++)
+			if (SA12[i] < n0)
+				R0[j++] = 3 * SA12[i];
+		radixPass(R0, SA0, T, n0, K);
+		for (int p = 0, t = n0 - n1, k = 0; k < n; k++) {
+#define GetI() (SA12[t] < n0 ? SA12[t] * 3 + 1 : (SA12[t] - n0) * 3 + 2)
+			int i = GetI(); // pos of current offset 12 suffix
+			int j = SA0[p]; // pos of current offset 0 suffix
+			if (SA12[t] < n0 ? // different compares for mod 1 and mod 2 suffixes
+			leq(T[i], R[SA12[t] + n0], T[j], R[j / 3]) : 
+			leq(T[i], T[i + 1], R[SA12[t] - n0 + 1], T[j], T[j + 1], R[j / 3 + n0])) { // suffix from SA12 is smaller
+				SA[k] = i;
+				t++;
+				if (t == n02) // done --- only SA0 suffixes left
+					for (k++; p < n0; p++, k++)
+						SA[k] = SA0[p];
+			} else { // suffix from SA0 is smaller
+				SA[k] = j;
+				p++;
+				if (p == n0) // done --- only SA12 suffixes left
+					for (k++; t < n02; t++, k++)
+						SA[k] = GetI();
+			}
+		}
+		delete[] R;
+		delete[] SA12;
+		delete[] SA0;
+		delete[] R0;
 	}
 	void BuildLCP() {
 		VI PLCP(N), PHI(N);
