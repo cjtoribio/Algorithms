47eb96030865d6e53e5a64ebceca89c4e8f6eb3e
cjtoribio NA <cjtoribio@gmail.com>
2017-10-16 07:09:31 +0000

    HungarianAlgorithm.cpp edited online with Bitbucket

--- Codes/Graph/HungarianAlgorithm.cpp
+++ Codes/Graph/HungarianAlgorithm.cpp
@@ -1,65 +1,41 @@
-struct Hungarian
-{
-	VI assign(VVI mat) 
-	{
-		int N = mat.size();
-		VI R(N , -1) , C(N , -1) , DEL_R(N) , DEL_C(N);
-		for(int i = 0; i < N; ++i) // STEP 1
-		{
-			int mi = 1000000000;
-			for(int j = 0; j < N; ++j) mi = min(mi , mat[i][j]);
-			for(int j = 0; j < N; ++j) mat[i][j] -= mi;
-		}
-		for(int j = 0; j < N; ++j) // STEP 2
-		{
-			int mi = 1000000000;
-			for(int i = 0; i < N; ++i) mi = min(mi , mat[j][i]);
-			for(int i = 0; i < N; ++i) mat[j][i] -= mi;
-		}
-		while(true)
-		{
-			queue<int> Q;
-			for(int i = 0; i < N; ++i)
-			{
-				for(int j = 0; j < N; ++j)
-				{
-					if(mat[i][j] == 0 && C[j] == -1)
-					{
-						C[j] = i;
-						R[i] = j;
-						break;
-					}
+struct Hungarian {
+	VI matchL, matchR;
+	int cost;
+	int run(VVI &a){
+		const int n = a.size(), m = a[0].size(), INF = 1000000000;
+		vector<int> u(n + 1), v(m + 1), p(m + 1), way(m + 1);
+		for (int i=1; i<=n; ++i) {
+			p[0] = i;
+			int j0 = 0;
+			vector<int> minv (m+1, INF);
+			vector<bool> used (m+1, false);
+			do {
+				used[j0] = true;
+				int i0 = p[j0], delta = INF, j1;
+				for (int j=1; j<=m; ++j)
+				if (!used[j]) {
+					int cur = a[i0-1][j-1]-u[i0]-v[j];
+					if (cur < minv[j])
+					minv[j] = cur, way[j] = j0;
+					if (minv[j] < delta)
+					delta = minv[j], j1 = j;
 				}
-				if(R[i] == -1)Q.push(i);
-			}
-			VI MARK_C(N);
-			if(Q.size() == 0)return C;
-			while(Q.empty())
-			{
-				int i = Q.front(); Q.pop();
-				for(int j = 0; j < N; ++j)
-				{
-					if(C[j] != -1 && mat[i][j] == 0)
-					{
-						Q.push(R[i]);
-						R[C[j]] = -1; 
-						C[j] = -1;
-						MARK_C[j] = 1;
-					}
-				}
-			}
-			int mi = 1000000000;
-			for(int i = 0; i < N; ++i)
-				for(int j = 0; j < N; ++j)
-					if(R[i] == -1 && !MARK_C[j])
-						mi = min(mi , mat[i][j]);
-			for(int i = 0; i < N; ++i)
-				for(int j = 0; j < N; ++j)
-					if(R[i] != -1 && MARK_C[j])
-						mat[i][j] += mi;
-					else if(R[i] == -1 || !MARK_C[j])
-						mat[i][j] -= mi;
+				for (int j=0; j<=m; ++j)
+				if (used[j])
+				u[p[j]] += delta, v[j] -= delta;
+				else
+				minv[j] -= delta;
+				j0 = j1;
+			}while (p[j0] != 0);
+			do {
+				int j1 = way[j0];
+				p[j0] = p[j1];
+				j0 = j1;
+			}while (j0);
 		}
-		return VI(N);
+		matchL = VI(n, -1), matchR = VI(m);
+		for (int j=1; j<=m; ++j) if(p[j]) matchL[p[j]-1] = j-1;
+		for (int j=1; j<=m; ++j) matchR[j-1] = p[j]-1;
+		return cost = -v[0];
 	}
 };
