14264c661bf3b57d83db578e67f0abcfc3245ff5
Carlos Toribio <cjtoribio@gmail.com>
2017-10-19 02:39:15 -0400

    Otimized memory
    Implemented otpimized memory trick and added reference.

--- Codes/DynamicProgramming/SubsetSum.cpp
+++ Codes/DynamicProgramming/SubsetSum.cpp
@@ -1,34 +1,31 @@
 typedef vector<int> VI;
 typedef vector<VI> VVI;
 typedef long long Long;
+// http://codeforces.com/blog/entry/45223
 struct SubsetSum {
 	int mb;
-	VVI A; // A[j][i] = subset   of "i" on the lower "j" bits and match exacly upper bits
-	VVI B; // B[j][i] = superset of "i" on the lower "j" bits and match exacly upper bits;
-	SubsetSum(int mb):mb(mb), A(mb+1, VI(1<<mb)), B(mb+1, VI(1<<mb)){}
+	VI A, B;
+	SubsetSum(int mb):mb(mb), A(1<<mb), B(1<<mb){}
 	void addVal(int n, int v = 1){
-		A[0][n] += v; // for counting subsets
-		B[0][n] += v; // for counting supersets
+		A[n] += v; // for counting subsets
+		B[n] += v; // for counting supersets
 	}
 	void build(){
 		int ALL = (1<<mb)-1;
-		for (int i = 0; i < (1<<mb); ++i) {
-			for (int j = 0; j < mb; ++j) {
+		for (int j = 0; j < mb; ++j) {
+			for (int i = 0; i < (1<<mb); ++i) {
 				if(i & (1<<j) ) {
-					A[j+1][i] = A[j][i] + A[j][i^(1<<j)];
-					B[j+1][ALL-i] = B[j][ALL-i] + B[j][(ALL-i)^(1<<j)];
-				}else{
-					B[j+1][ALL-i] = B[j][ALL-i];
-					A[j+1][i] = A[j][i];
+					A[i] += A[i^(1<<j)];
+					B[ALL-i] += B[(ALL-i)^(1<<j)];
 				}
 			}
 		}
 	}
 	int sumSupersets(int n){
-		return B[mb][n];
+		return B[n];
 	}
 	int sumSubsets(int n){
-		return A[mb][n];
+		return A[n];
 	}
 	int bestOr(int n){
 		int best = 0;
