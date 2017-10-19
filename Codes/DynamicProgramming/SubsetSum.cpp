06eea56c841650a580c64ea46da015d3129c871d
cjtoribio NA <cjtoribio@gmail.com>
2017-10-19 06:20:03 +0000

    Added comment to SubsetSum computation.

--- Codes/DynamicProgramming/SubsetSum.cpp
+++ Codes/DynamicProgramming/SubsetSum.cpp
@@ -1,20 +1,21 @@
 struct SubsetSum {
 	int mb;
-	VVI A, B;
+	VVI A; // A[j][i] = subset   of "i" on the lower "j" bits and match exacly upper bits
+	VVI B; // B[j][i] = superset of "i" on the lower "j" bits and match exacly upper bits;
 	SubsetSum(int mb):mb(mb), A(mb+1, VI(1<<mb)), B(mb+1, VI(1<<mb)){}
 	void addVal(int n, int v = 1){
 		A[0][n] += v; // for counting subsets
 		B[0][n] += v; // for counting supersets
 	}
 	void build(){
-		int LAST = (1<<mb)-1;
+		int ALL = (1<<mb)-1;
 		for (int i = 0; i < (1<<mb); ++i) {
 			for (int j = 0; j < mb; ++j) {
 				if(i & (1<<j) ) {
 					A[j+1][i] = A[j][i] + A[j][i^(1<<j)];
-					B[j+1][LAST-i] = B[j][LAST-i] + B[j][(LAST-i)^(1<<j)];
+					B[j+1][ALL-i] = B[j][ALL-i] + B[j][(ALL-i)^(1<<j)];
 				}else{
-					B[j+1][LAST-i] = B[j][LAST-i];
+					B[j+1][ALL-i] = B[j][ALL-i];
 					A[j+1][i] = A[j][i];
 				}
 			}
