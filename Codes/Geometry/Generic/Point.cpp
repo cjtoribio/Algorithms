8f2026ab5fadc1f0c0edb06eaef9336938378d07
Carlos Toribio <cjtoribio@gmail.com>
2017-11-16 03:57:35 -0400

    Added PointStruct from AlCash
    Copied point struct from AlCash very good for generic point

---
+++ Codes/Geometry/Generic/Point.cpp
@@ -0,0 +1,41 @@
+template <class F>
+struct Point {
+  F x, y;
+  Point() : x(0), y(0) {}
+  Point(const F& x, const F& y) : x(x), y(y) {}
+
+  template <class F1> 
+  Point& operator = (const Point<F1>& o) { x = o.x; y = o.y; return *this; }
+  template <class F1> explicit operator Point<F1> () const {
+    return Point<F1>(static_cast<F1>(x), static_cast<F1>(y)); }
+  
+};
+
+template <class F> inline Point<F> makePoint(const F& x, const F& y) { return Point<F>(x, y); }
+
+#define FUNC1(name, arg, expr) \
+template <class F> inline auto name(const arg) -> decltype(expr) { return expr; }
+#define FUNC2(name, arg1, arg2, expr) \
+template <class F1, class F2> \
+inline auto name(const arg1, const arg2) -> decltype(expr) { return expr; }
+#define FUNC3(name, arg1, arg2, arg3, expr) \
+template <class F1, class F2, class F3> \
+inline auto name(const arg1, const arg2, const arg3) -> decltype(expr) { return expr; }
+
+FUNC1(operator -, Point<F>& p,    makePoint(-p.x, -p.y))
+FUNC1(gcd,        Point<F>& p,    p / abs(__gcd(p.x, p.y)) )
+FUNC2(operator +, Point<F1>& lhs, Point<F2>& rhs, makePoint(lhs.x + rhs.x, lhs.y + rhs.y))
+FUNC2(operator -, Point<F1>& lhs, Point<F2>& rhs, makePoint(lhs.x - rhs.x, lhs.y - rhs.y))
+FUNC2(operator *, F1& factor,     Point<F2>& rhs, makePoint(factor * rhs.x, factor * rhs.y))
+FUNC2(operator *, Point<F1>& lhs, F2& factor, makePoint(lhs.x * factor, lhs.y * factor))
+FUNC2(operator /, Point<F1>& lhs, F2& factor, makePoint(lhs.x / factor, lhs.y / factor))
+FUNC2(operator *, Point<F1>& lhs, Point<F2>& rhs, lhs.x * rhs.x + lhs.y * rhs.y)
+FUNC2(operator ^, Point<F1>& lhs, Point<F2>& rhs, lhs.x * rhs.y - lhs.y * rhs.x)
+
+FUNC2(operator ==, Point<F1>& lhs, Point<F2>& rhs, lhs.x == rhs.x && lhs.y == rhs.y)
+FUNC2(operator !=, Point<F1>& lhs, Point<F2>& rhs, !(lhs == rhs))
+FUNC2(operator <,  Point<F1>& lhs, Point<F2>& rhs, lhs.x < rhs.x || (lhs.x == rhs.x && lhs.y < rhs.y))
+
+#undef FUNC1
+#undef FUNC2
+#undef FUNC3
