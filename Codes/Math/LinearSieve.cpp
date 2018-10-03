dc9ff6d115ff6cd8ab2c45bd65d0e557adf9088b
Carlos Toribio <cjtoribio@gmail.com>
2018-10-03 17:58:01 +0100

    Update LinearSieve.cpp

--- Codes/Math/LinearSieve.cpp
+++ Codes/Math/LinearSieve.cpp
@@ -1,18 +1,18 @@
 struct LinearSieve {
-  VI P, C;
-  LinearSieve(int L): C(L+1) { 
-    for (int i = 2; i < C.size(); ++i) {
-      if (C[i] == 0) {
-          C[i] = i;
-          P.push_back (i);
+  VI primes, primeFactor;
+  LinearSieve(int L): primeFactor(L+1) { 
+    for (int i = 2; i < primeFactor.size(); ++i) {
+      if (primeFactor[i] == 0) {
+          primeFactor[i] = i;
+          primes.push_back (i);
       }
-      for (int j=0; j < P.size() && P[j]<=C[i] && i*P[j]<=L; ++j)
-          C[i * P[j]] = P[j];
+      for (int j=0; j < primes.size() && primes[j]<=primeFactor[i] && i*primes[j]<=L; ++j)
+          primeFactor[i * primes[j]] = primes[j];
     }
   }
   VI factors(int n) {
     VI F;
-    while (n > 1) F.push_back(C[n]), n /= C[n];
+    while (n > 1) F.push_back(primeFactor[n]), n /= primeFactor[n];
     return F;
   }
   void rec(vector<int> &V, int tp, int id, int c, VI &A) {
