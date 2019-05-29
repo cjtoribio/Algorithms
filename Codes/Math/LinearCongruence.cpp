65025f3478ffcc319224992a6cf2ba9a0da05668
Carlos Toribio <cjtoribio@gmail.com>
2019-05-29 12:19:00 +0200

    Update LinearCongruence.cpp

--- Codes/Math/LinearCongruence.cpp
+++ Codes/Math/LinearCongruence.cpp
@@ -1,16 +1,10 @@
-// a*x = b (mod m)
+// a*x = b (mod m) 
+// -1 (no solution if gcd(a, m) dont divide b) or x (a solution)
+// there exist gcd(a, m) solutions all of which are x + m/gcd(a,m)*[0...gcd-1]
 int solveLinearCongruence(int a, int b, int m) {
 	if (b == 0) return 0;
 	if (a == 0) return -1;
 	if (a == 1) return b;
 	int y = solveLinearCongruence(m % a, (-b % a + a) % a, a);
 	return y < 0 ? y : (1LL*m*y+b) / a % m;
-}
-VI allSolutions(int a, int b, int m) {
-	int x = solveLinearCongruence(a, b, m);
-	if (x == -1) return {};
-	VI V;
-	int gc = __gcd(a, m);
-	for (int i = 0; i < gc; ++i) V.push_back(x + m / gc * i);
-	return V;
 }
