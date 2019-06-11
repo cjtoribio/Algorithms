c7695776da2ecc87df6934f8590218e3ecd26678
Carlos Toribio <cjtoribio@gmail.com>
2019-06-12 00:30:48 +0100

    Optimized reducing one mod

--- Codes/Math/LinearCongruence.cpp
+++ Codes/Math/LinearCongruence.cpp
@@ -2,9 +2,9 @@
 // a*x = b (mod m) 
 // -1 (no solution if gcd(a, m) dont divide b) or x (a solution)
 // there exist gcd(a, m) solutions all of which are x + m/gcd(a,m)*[0...gcd-1]
-int solveLinearCongruence(int a, int b, int m) {
-	if (b == 0) return 0;
+int solveLinearCongruence(int a, int b, int m) { // ~2M per second
+	if (b == 0 || b == m) return 0;
 	if (a == 0) return -1;
-	int y = solveLinearCongruence(m % a, (a - b % a) % a, a);
+	int y = solveLinearCongruence(m % a, -b % a + a, a);
 	return y < 0 ? y : (1LL*m*y+b) / a;
 }
